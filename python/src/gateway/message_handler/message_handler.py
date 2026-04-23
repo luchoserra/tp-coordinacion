e99220b709b12ebacd2aa25286edce674d578688
luciano <luchoserra288@gmail.com>
2026-04-23 04:14:20 -0300

    fix: coordinate sum flush across workers using record count from gateway

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -6,13 +6,17 @@ class MessageHandler:
 
     def __init__(self):
         self.client_id = uuid.uuid4().hex
+        self.record_count = 0
 
     def serialize_data_message(self, message):
         [fruit, amount] = message
+        self.record_count += 1
         return message_protocol.internal.serialize([self.client_id, fruit, amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([self.client_id])
+        return message_protocol.internal.serialize(
+            [self.client_id, self.record_count]
+        )
 
     def deserialize_result_message(self, message):
         client_id, fruit_top = message_protocol.internal.deserialize(message)
--- python/src/sum/main.py
+++ python/src/sum/main.py
@@ -15,6 +15,9 @@ SUM_CONTROL_EXCHANGE = "SUM_CONTROL_EXCHANGE"
 AGGREGATION_AMOUNT = int(os.environ["AGGREGATION_AMOUNT"])
 AGGREGATION_PREFIX = os.environ["AGGREGATION_PREFIX"]
 
+PREFLUSH = "PREFLUSH"
+COUNT = "COUNT"
+
 
 class SumFilter:
 
@@ -22,19 +25,26 @@ class SumFilter:
         self.input_queue = middleware.MessageMiddlewareQueueRabbitMQ(
             MOM_HOST, INPUT_QUEUE
         )
-        self.eof_broadcast = None
-        self.eof_input = middleware.MessageMiddlewareExchangeRabbitMQ(
+        self.control_input = middleware.MessageMiddlewareExchangeRabbitMQ(
             MOM_HOST, SUM_CONTROL_EXCHANGE, [f"{SUM_CONTROL_EXCHANGE}_{ID}"]
         )
-        self.data_output_exchanges = []
-        for i in range(AGGREGATION_AMOUNT):
-            self.data_output_exchanges.append(
-                middleware.MessageMiddlewareExchangeRabbitMQ(
-                    MOM_HOST, AGGREGATION_PREFIX, [f"{AGGREGATION_PREFIX}_{i}"]
-                )
+        self.control_output = middleware.MessageMiddlewareExchangeRabbitMQ(
+            MOM_HOST,
+            SUM_CONTROL_EXCHANGE,
+            [f"{SUM_CONTROL_EXCHANGE}_{i}" for i in range(SUM_AMOUNT)],
+        )
+        self.aggregator_outputs = [
+            middleware.MessageMiddlewareExchangeRabbitMQ(
+                MOM_HOST, AGGREGATION_PREFIX, [f"{AGGREGATION_PREFIX}_{i}"]
             )
+            for i in range(AGGREGATION_AMOUNT)
+        ]
 
-        self.fruit_amounts_by_client = {}
+        self.fruit_totals = {}
+        self.local_count = {}
+        self.expected_total = {}
+        self.reported = {}
+
         self.lock = threading.Lock()
 
         signal.signal(signal.SIGTERM, self._handle_sigterm)
@@ -42,73 +52,98 @@ class SumFilter:
     def _handle_sigterm(self, signum, frame):
         logging.info("SIGTERM received")
         self.input_queue.stop_consuming()
-        self.eof_input.stop_consuming()
+        self.control_input.stop_consuming()
 
-    def _get_aggregator_index(self, fruit):
+    def _aggregator_for(self, fruit):
         return int(hashlib.md5(fruit.encode()).hexdigest(), 16) % AGGREGATION_AMOUNT
 
-    def _process_data(self, client_id, fruit, amount):
-        with self.lock:
-            client_fruits = self.fruit_amounts_by_client.setdefault(client_id, {})
-            client_fruits[fruit] = client_fruits.get(
-                fruit, fruit_item.FruitItem(fruit, 0)
-            ) + fruit_item.FruitItem(fruit, int(amount))
+    def _broadcast(self, payload):
+        self.control_output.send(message_protocol.internal.serialize(payload))
 
-    def _flush_client(self, client_id):
-        with self.lock:
-            fruit_amounts = self.fruit_amounts_by_client.pop(client_id, {})
+    def _record_fruit(self, client_id, fruit, amount):
+        client_fruits = self.fruit_totals.setdefault(client_id, {})
+        client_fruits[fruit] = client_fruits.get(
+            fruit, fruit_item.FruitItem(fruit, 0)
+        ) + fruit_item.FruitItem(fruit, int(amount))
+        self.local_count[client_id] = self.local_count.get(client_id, 0) + 1
+
+    def _flush(self, client_id):
+        fruits = self.fruit_totals.pop(client_id, {})
+        self.local_count.pop(client_id, None)
+        self.expected_total.pop(client_id, None)
+        self.reported.pop(client_id, None)
 
         logging.info(f"Flushing results for client {client_id}")
-        for fi in fruit_amounts.values():
-            idx = self._get_aggregator_index(fi.fruit)
-            self.data_output_exchanges[idx].send(
+        for fi in fruits.values():
+            self.aggregator_outputs[self._aggregator_for(fi.fruit)].send(
                 message_protocol.internal.serialize([client_id, fi.fruit, fi.amount])
             )
-        for exchange in self.data_output_exchanges:
+        for exchange in self.aggregator_outputs:
             exchange.send(message_protocol.internal.serialize([client_id]))
+
+    def _try_flush(self, client_id):
+        if client_id not in self.expected_total:
+            return
+        reported = self.reported.get(client_id, {})
+        if len(reported) < SUM_AMOUNT:
+            return
+        if sum(reported.values()) != self.expected_total[client_id]:
+            return
+        self._flush(client_id)
 
     def process_data_message(self, message, ack, nack):
         try:
             fields = message_protocol.internal.deserialize(message)
-            if len(fields) == 3:
-                self._process_data(*fields)
-            else:
-                client_id = fields[0]
-                self.eof_broadcast.send(
-                    message_protocol.internal.serialize([client_id])
-                )
+            with self.lock:
+                if len(fields) == 3:
+                    client_id, fruit, amount = fields
+                    self._record_fruit(client_id, fruit, amount)
+                    if client_id in self.expected_total:
+                        self._broadcast(
+                            [COUNT, client_id, ID, self.local_count[client_id]]
+                        )
+                else:
+                    client_id, total = fields
+                    self._broadcast([PREFLUSH, client_id, total])
             ack()
         except Exception:
             nack()
             raise
 
-    def process_eof_message(self, message, ack, nack):
+    def process_control_message(self, message, ack, nack):
         try:
             fields = message_protocol.internal.deserialize(message)
-            client_id = fields[0]
-            self._flush_client(client_id)
+            tag = fields[0]
+            with self.lock:
+                if tag == PREFLUSH:
+                    _, client_id, total = fields
+                    self.expected_total[client_id] = total
+                    self._broadcast(
+                        [COUNT, client_id, ID, self.local_count.get(client_id, 0)]
+                    )
+                elif tag == COUNT:
+                    _, client_id, sender_id, count = fields
+                    reported = self.reported.setdefault(client_id, {})
+                    reported[sender_id] = max(reported.get(sender_id, 0), count)
+                    self._try_flush(client_id)
             ack()
         except Exception:
             nack()
             raise
 
-    def _data_thread(self):
-        self.eof_broadcast = middleware.MessageMiddlewareExchangeRabbitMQ(
-            MOM_HOST,
-            SUM_CONTROL_EXCHANGE,
-            [f"{SUM_CONTROL_EXCHANGE}_{i}" for i in range(SUM_AMOUNT)],
-        )
-        self.input_queue.start_consuming(self.process_data_message)
-        self.input_queue.close()
-        self.eof_broadcast.close()
-
     def start(self):
-        t = threading.Thread(target=self._data_thread)
-        t.start()
-        self.eof_input.start_consuming(self.process_eof_message)
-        self.eof_input.close()
-        t.join()
-        for exchange in self.data_output_exchanges:
+        data_thread = threading.Thread(
+            target=self.input_queue.start_consuming,
+            args=(self.process_data_message,),
+        )
+        data_thread.start()
+        self.control_input.start_consuming(self.process_control_message)
+        data_thread.join()
+
+        self.input_queue.close()
+        self.control_input.close()
+        self.control_output.close()
+        for exchange in self.aggregator_outputs:
             exchange.close()
 
 
